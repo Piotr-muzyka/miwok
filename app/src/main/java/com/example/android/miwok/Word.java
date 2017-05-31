4ae3c2433a3fa52f27c32e1828c750e5aabb61ed
Piotr Muzyka <piotr.muzyka@capgemini.com>
2017-05-31 16:41:31 +0200

    changes after lesson 5.12

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,17 +10,30 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int resourceId;
 
 
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation= defaultTranslation;
         mMiwokTranslation= miwokTranslation;
     }
+
+    // Overloading constructor - multiple constructors for a Java class
+    //in this scenario we need one constructor with 2 TextViews and an ImageView in order to create ListView for most activities
+    // and a second constructor for Phrases activity which has no ImageView - constructor without mResourceId.
+
+    public Word(String defaultTranslation, String miwokTranslation, int mResourceId){
+        mDefaultTranslation= defaultTranslation;
+        mMiwokTranslation= miwokTranslation;
+        resourceId = mResourceId;
+    }
     public String getEnglishTranslation(){
         return mDefaultTranslation;
     }
-
     public String getMiwokTranslation(){
         return mMiwokTranslation;
+    }
+    public int getResourceId() {
+        return resourceId;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -71,6 +72,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
         miwokTextView.setText(currentWord.getMiwokTranslation());
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
+
+        ImageView imageRepresentation = (ImageView) listItemView.findViewById(R.id.imageView);
+        imageRepresentation.setImageResource(currentWord.getResourceId());
         return listItemView;
     }
 }
